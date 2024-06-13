cc1bb5ef9f453ee7007a77ef5af605d43e58abda
hyerin11 <hodolin24@naver.com>
2024-06-13 11:23:48 +0900

    #practice _ 체크박스 애니메이션

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import ExpenseList from './components/expenses/ExpenseList';
 import NewExpense from './components/new-expense/NewExpense';
+import CheckBoxStyle from './components/practice/CheckBoxStyle'
 
 const App = () => {
 
@@ -26,6 +27,7 @@ const App = () => {
 
   return (
     <>
+      <CheckBoxStyle />
       <NewExpense />
       <ExpenseList expenses={expenses} />
     </>
