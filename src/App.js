25dae106bc3a4f55e92b5ba51e7caee04296271d
hyerin11 <hodolin24@naver.com>
2024-06-14 09:23:21 +0900

    # 목록추가 후 리스트 나열(해당 년에 맞는 리스트 출력)

--- src/App.js
+++ src/App.js
@@ -1,63 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ExpenseList from './components/expenses/ExpenseList';
 import NewExpense from './components/new-expense/NewExpense';
-
 
 const App = () => {
 
   // 서버에서 지출항목 JSON 배열을 응답받음
   const expenses = [
     {
-      title: '치킨',
+      title: '치킨먹음',
       price: 30000,
       date: new Date(2024, 6 - 1, 3)
     },
     {
-      title: '족발',
+      title: '족발먹음',
       price: 40000,
       date: new Date(2023, 12 - 1, 17)
     },
     {
-      title: '헬스장',
+      title: '헬스장등록',
       price: 300000,
       date: new Date(2024, 6 - 1, 12)
     },
     {
-      title: '피자',
-      price: 10000,
+      title: '파파존스피자',
+      price: 40000,
       date: new Date(2022, 3 - 1, 4)
     },
     {
-      title: '사탕',
-      price: 500,
-      date: new Date(2023, 1 - 1, 1)
+      title: '파리채',
+      price: 1500,
+      date: new Date(2023, 5 - 1, 6)
     },
   ];
 
-
-//ExpenseFrom에게 내려보낼 함수
-const onAddExpense = (userInput) => {
-  console.log('App.js가 내려보낸 함수 호출!');
-  //console.log(userInput);
-
-  expenses.push(userInput);
-  console.log(expenses);
-};
+  // 배열을 상태변수로 관리
+  const [expenseList, setExpenseList] = useState(expenses); 
 
 
-//ExpenseFilter에게 내려보낼 함수=================
-// const onAdd = (userInput) => {
-//   console.timeLog('App.js가 내려보낸 함수이다');
-
-//   filter.push(userInput);
-//   console.log(filter);
-// }
+  // ExpenseForm에게 내려보낼 함수
+  const onAddExpense = (userInput) => setExpenseList([...expenseList, userInput]);
 
   return (
     <>
       <NewExpense onSave={onAddExpense} />
-      <ExpenseList expenses={expenses} />
+      <ExpenseList expenses={expenseList} />
     </>
   );
 };
