73a839d67baa86e4cd56cc20b86e08f9e8b7faa5
hyerin11 <hodolin24@naver.com>
2024-06-17 11:41:41 +0900

    # 에러창(이름 null, 나이 음수값)

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -2,24 +2,25 @@ import React, { useState } from 'react';
 import styles from './AddUsers.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/Modal/ErrorModal';
 
-// AddUsers 컴포넌트는 사용자 추가 폼을 렌더링하고 사용자 데이터를 부모 컴포넌트로 전달합니다.
 const AddUsers = ({ onAddUser }) => {
-  // useState를 사용하여 사용자 이름과 나이 상태를 관리합니다.
+
+  // 에러 상태 관리 (에러 제목, 에러 내용)
+  const [error, setError] = useState(null);
+
   const [userValue, setUserValue] = useState({
     username: '',
     age: '',
   });
 
-  // 사용자 이름 변경 핸들러
-  const usernameChangeHandler = (e) => { 
+  const usernameChangeHandler = (e) => {
     setUserValue((prevUserValue) => ({
       ...prevUserValue,
       username: e.target.value,
     }));
   };
 
-  // 사용자 나이 변경 핸들러
   const ageChangeHandler = (e) => {
     setUserValue((prevUserValue) => ({
       ...prevUserValue,
@@ -27,30 +28,43 @@ const AddUsers = ({ onAddUser }) => {
     }));
   };
 
-  // 폼 제출 핸들러
   const userSubmitHandler = (e) => {
     e.preventDefault();
 
     // 입력값 검증
     if (userValue.username.trim() === '' || userValue.age.trim() === '') {
+      setError({
+        title: '유효하지 않은 입력값',
+        message: '입력값은 공백으로 작성하면 안됩니다.'
+      });
       return;
     }
 
-    // 나이 검증 (1 미만의 값은 허용하지 않음)
-    if (+userValue.age < 1) return;
+    if (+userValue.age < 1) {
+      setError({
+        title: '유효하지 않은 나이 범위',
+        message: '나이는 0이나 음수가 될 수 없습니다.'
+      });
+      return;
+    }
 
-    // 유효한 사용자 데이터를 부모 컴포넌트로 전달
     onAddUser(userValue);
 
-    // 폼 초기화
     setUserValue({
       username: '',
       age: '',
     });
   };
 
+  // 하위 컴포넌트 ErrorModal에게 모달을 닫는 조건을 수행하는 함수 전달
+  const modalCloseHandler = () => {
+    setError(null);
+  } ;
+
+
   return (
     <> 
+      { error && <ErrorModal title={error.title} message={error.message} onClose={modalCloseHandler}/> }
       <Card className={styles.input}>
         <form onSubmit={userSubmitHandler}>
           <label htmlFor="username">이름</label>
