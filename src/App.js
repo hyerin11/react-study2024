439a4d62ff2e85f1c6649da11ec945c055acb097
hyerin11 <hodolin24@naver.com>
2024-06-17 12:43:36 +0900

    # useRef로 DOM조작하기

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ const App = () => {
 // 회원들이 저장될 배열
   const [userList, setUserList] = useState([]); //빈 배열인데
 
+  // let cnt = 1; 일반변수는 절대 올 수 없다.
+
   const count = useRef(1);
 
   const addUserHandler = user => { //신규 유저를 넣으면
     
-    count.current++;
     console.log('count: ',count.current);
     setUserList(prev => [
       ...prev, //기존 회원 복사해와서
