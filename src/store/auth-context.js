c82f6477c332bed15c58560a4129b562ee7e85e4
hyerin11 <hodolin24@naver.com>
2024-06-19 09:33:29 +0900

    # Context API 기초

---
+++ src/store/auth-context.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// 로그인 관련 상태데이터들을 중앙집중 관리하는 파일
+// 컨텍스트는 데이터 중앙저장소
+// : 생성시 관리할 기본 데이터를 명시해야 한다.
+// 데이터 값은 타입 기본값을 명시해야 한다.
+const AuthContext = React.createContext(
+    {
+        isLoggedIn: false
+
+    }
+);
+
+
+
+export default AuthContext;
