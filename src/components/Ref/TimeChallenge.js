7d25a45d0c243b540e4052d5f8b3ac181e5208ca
hyerin11 <hodolin24@naver.com>
2024-06-20 11:20:05 +0900

    # useRef를 이용해 해결(timer 공유 방지)

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-
-let timer; //전역변수라서. 하나를 4개가 공유함. ()
+import React, { useRef, useState } from 'react';
 
 const TimerChallenge = ({ title, targetTime }) => {
+
+  // timer를 ref변수로 관리
+  const timer = useRef();
 
   // 타이머가 시작되었는지를 확인하는 상태값
   const [timerStarted, setTimerStarted] = useState(false);
@@ -10,26 +11,23 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 타겟시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
-  
   const startHandler = e => {
 
-    timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
-    }, targetTime * 1000); //초로 보내지만 밀리초로 받았기 때문에 *1000
+    }, targetTime * 1000);
 
     setTimerStarted(true);
+  };
+
+  const stopHandler = e => {
+    clearTimeout(timer.current);
   };
 
   //전역변수로 timer 설정시 5초 -> 1초 -> 1초 -> 5초 클릭하면
   //5초짜리 timer가 정상 작동하지 않는다.
   //그 이유느, 4개의 TimerChallenge컴포넌트가 1개의 timer를 공유하기 때문에
   //처음 5초짜리 timer가 1초짜리 timer에 의해 덮어씌워지기 때문이다.
-
-const stopHandler = e => { //버튼은 하나인데, start와 stop 핸들러가 2개일 때
-    console.log('stop!')
-    clearTimeout(timer);
-  }
-
   return (
     <section className="challenge">
       <h2>{title}</h2>
