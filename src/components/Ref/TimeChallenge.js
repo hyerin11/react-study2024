91851ba3e98788624bc5a8e9a0fdc3f2d91a3166
hyerin11 <hodolin24@naver.com>
2024-06-20 12:19:46 +0900

    #남은 시간 초 보여주기, 타임오버되었을 시 종료

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -11,26 +11,37 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 타겟시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
+  //stop을 눌렀을 때 남은시간을 상태값으로 관리
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
   // 자식컴포넌트 ResultModal에 있는 dialog태그의 참조를 만듬
   const dialog = useRef();
 
+  // 시간이 타임오버되었을 때 처리
+  if(timeRemaining <= 0){
+    clearInterval(timer.current);
+    dialog.current.showModal();
+  }
+
   const startHandler = (e) => {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      // modal open!
-      dialog.current.showModal();
-    }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimeRemaining(prevTime => prevTime - 10);
+    }, 10);
 
     setTimerStarted(true);
   };
 
   const stopHandler = (e) => {
-    clearTimeout(timer.current);
+    clearInterval(timer.current);
+    dialog.current.showModal();
   };
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal ref={dialog}
+                    targetTime={targetTime}
+                    remainingTime={timeRemaining}
+                    result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
