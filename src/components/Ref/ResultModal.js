e085ea2f2fab93d5390edf2fc1e31bd057edf1e1
hyerin11 <hodolin24@naver.com>
2024-06-20 11:41:40 +0900

    # dialog 태그 제어하기

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -1,15 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
-const ResultModal = ({result, targetTime}) => {
-
-  const dialog = useRef();
-
-  useEffect(() => {
-    dialog.current.showModal();
-  }, []);
+const ResultModal = ({ref, result, targetTime}) => {
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={ref} className="result-modal">
       <h2>Your {result}!</h2>
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the timer with <strong>X seconds left.</strong></p>
