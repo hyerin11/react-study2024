bc0a269134317059acc413c144c7759da3c29c3c
hyerin11 <hodolin24@naver.com>
2024-06-21 09:31:35 +0900

    #Food_장바구니(Cart App) 아이콘 추가

--- src/components/Food/Layout/Header.js
+++ src/components/Food/Layout/Header.js
@@ -3,6 +3,7 @@ import styles from './Header.module.scss';
 
 //정적 이미지 로딩
 import foodImage from'../../../assets/img/meals.jpg'; //변수명을 써줘야 한다.
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
   //console.log('styles: ', styles);
@@ -13,6 +14,7 @@ const Header = () => {
     <>
     <header className={header}>
         <h1>ReactMeals</h1>    
+        <HeaderCartButton />
     </header>
     <div className={mainImage}>
         <img src={foodImage} alt='음식 사진'/>
