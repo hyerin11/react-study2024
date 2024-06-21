e20ec709e57137d2459a4bee565006eede7bdeea
hyerin11 <hodolin24@naver.com>
2024-06-21 09:25:39 +0900

    #food_css적용 및 사진 넣기

--- src/components/Food/Layout/Header.js
+++ src/components/Food/Layout/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import styles from './Header.module.scss';
+
+//정적 이미지 로딩
+import foodImage from'../../../assets/img/meals.jpg'; //변수명을 써줘야 한다.
 
 const Header = () => {
+  //console.log('styles: ', styles);
+
+  const {header, 'main-image': mainImage } = styles;
+
   return (
     <>
-    <header>
+    <header className={header}>
         <h1>ReactMeals</h1>    
     </header>
-    <div>
-        <img />
+    <div className={mainImage}>
+        <img src={foodImage} alt='음식 사진'/>
     </div>
     </>
   );
