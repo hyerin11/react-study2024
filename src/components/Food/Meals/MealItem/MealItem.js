4cbfaed5b0cd622326edf2b05b26636928dbb0d0
hyerin11 <hodolin24@naver.com>
2024-06-21 12:43:31 +0900

    # 장바구니 리듀서에 추가액션 제어하기

--- src/components/Food/Meals/MealItem/MealItem.js
+++ src/components/Food/Meals/MealItem/MealItem.js
@@ -3,33 +3,30 @@ import styles from './MealItem.module.scss';
 import MealItemForm from './MealItemForm';
 import CartContext from '../../../../store/cart-context';
 
-
 const MealItem = ({id, price, description, name}) => {
 
-  //Context에서 중앙관리되는 데이터들을 한번에 소비할 수 있는 Hook
+  // Context에서 중앙관리되는 데이터들을 한번에 소비할 수 있는 Hook
   const {addItem} = useContext(CartContext);
-  //console.log('ctx in MealItem', ctx);
-  
+  // console.log('ctx in MealItem: ', ctx);
 
   const { meal, description: desc, price: priceStyle } = styles;
 
   const formatPrice = new Intl.NumberFormat("ko-KR").format(price);
 
-//MealItemForm에서 선택한 수량값을 끌어올려줄 함수
-const addToCartHandler = (amount) => {
+  // MealItemForm에서 선택한 수량값을 끌어올려줄 함수
+  const addToCartHandler = (amount) => {
 
-  //장바구니에 보낼 객체
-  const item ={
-    id: id,
-    name: name,
-    amount: +amount,
-    price: price
+    // 장바구니에 보낼 객체
+    const item = {
+      id: id,
+      name: name,
+      amount: +amount,
+      price: price
+    };
+    // console.log('item: ', item);
+
+    addItem(item);
   };
-  console.log('item: ', item);
-
-  addItem(item);
-
-};
 
   return (
     <li className={meal}>
