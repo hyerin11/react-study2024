da1e4c5e818f6544c1dd7c93ac065ea89332f335
hyerin11 <hodolin24@naver.com>
2024-06-21 09:51:49 +0900

    # Meals 컴포넌트 담기

--- src/components/Food/Meals/AvailableMeals.js
+++ src/components/Food/Meals/AvailableMeals.js
@@ -31,10 +31,10 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((밥) => (
+  const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
