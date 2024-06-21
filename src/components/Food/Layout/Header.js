5c82c09d2c235e15afefd48523c22cf5dde68988
hyerin11 <hodolin24@naver.com>
2024-06-21 09:12:52 +0900

    # Food_파일 세팅

---
+++ src/components/Food/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+        <h1>ReactMeals</h1>    
+    </header>
+    <div>
+        <img />
+    </div>
+    </>
+  );
+};
+
+export default Header
