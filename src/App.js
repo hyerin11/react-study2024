120d894943ffa14740a8c894482dc956e87b16bc
hyerin11 <hodolin24@naver.com>
2024-07-02 12:10:55 +0900

    # 동적라우팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import Products from './components/RouteExample/pages/Products';
 import Home from './components/RouteExample/pages/Home';
+import Products from './components/RouteExample/pages/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './components/RouteExample/layout/RootLayout';
 import ErrorPage from './components/RouteExample/pages/ErrorPage';
+import ProductDetail from './components/RouteExample/pages/ProductDetail';
 
-//라우터 설정
+// 라우터 설정
 const router = createBrowserRouter([
 
-//중첩 라우터
   {
-    path: '/base',
+    path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '', element: <Home /> },
-      { path: 'products', element: <Products /> }
+      { index: true, element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'products/:prodId/page/:pageNo', element: <ProductDetail /> },
     ]
-  }
+  },
 
- 
 ]);
 
 const App = () => {
